59d9a704982f9778435b8ae3c3981b549179e491
hyverno <solo.hyverno@gmail.com>
2022-11-04 16:06:28 +0100

    fixe import javascript file

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@
 // add material on gltf
 
 import './style.css'
+import './app/main.js'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
